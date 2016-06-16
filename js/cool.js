120717b21ee0fad80ab733b414e0a464bd768286
Ari Katsoulis <arikatsoulis@icloud.com>
2016-06-16 13:27:47 +1200

    first commit

---
+++ js/cool.js
@@ -0,0 +1,19 @@
+
+
+
+$('#b3').on('click', function(){
+  	$("#bg").fadeOut(function() {
+    $(this).attr("src","https://thelightupnorth.files.wordpress.com/2014/07/2014-08-01-august-mood-board-contents.jpg").fadeIn(2000);
+  });
+});
+
+
+
+$('#img1').click(function() {
+
+  $("#imgmain").fadeOut(function() {
+    $(this).attr("src","img1.jpg").fadeIn();
+  });
+});
+
+	
